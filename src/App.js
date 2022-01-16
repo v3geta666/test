cf3fa8e6aba10ccc0965355c9a580e6c36c31d13
vegeta666 <angelochoa177@hotmail.com>
2022-01-16 00:33:12 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          a la verga me vale verga perro y jalese hijo de su puta madre <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React at vegeta666
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
